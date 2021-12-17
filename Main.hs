00d5bae0fa2642bcbc64fff1056d1af54624f4fd
dimchee <dimchee90@gmail.com>
2021-12-17 20:17:46 +0100

    Implement getFields using GHC.Generics

--- Main.hs
+++ Main.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TypeOperators, FlexibleContexts, FlexibleInstances, ScopedTypeVariables, TypeApplications #-}
 
-import Data.Aeson (FromJSON, Value (String))
-import GHC.Generics (Generic, U1, S1, (:*:), M1)
+import GHC.Generics
+import Data.Data (Proxy(..))
+import Data.Aeson
 import qualified Data.ByteString.Char8 as S8
 import Network.HTTP.Client (parseRequest, Request)
 import Network.HTTP.Types (hAccept, hAcceptLanguage, hUserAgent)
 import Network.HTTP.Simple (JSONException, httpJSONEither, getResponseBody, httpBS, setRequestHeader)
-import Data.Data (constrFields, Data (toConstr), dataTypeConstrs, dataTypeOf)
 import Data.List (intercalate)
-import Distribution.Compat.Lens
 import System.Process (readProcess)
 
 -- https://docs.invidious.io/API.md
@@ -19,25 +17,44 @@ data Video = Video {
     title :: String,
     description :: String,
     formatStreams :: [FormatStream]
-} deriving (Show, Eq, Generic, Data)
+} deriving (Show, Eq, Generic)
 instance FromJSON Video
 
 data FormatStream = FormatStream {
     url :: String,
     itag :: String,
-    --type :: String,
+    type_ :: String,
     quality :: String,
     container :: String,
     encoding :: String,
     qualityLabel :: String,
     resolution :: String,
     size :: String
-} deriving (Show, Eq, Generic, Data)
-instance FromJSON FormatStream
+} deriving (Show, Eq, Generic)
+instance FromJSON FormatStream where
+    parseJSON = genericParseJSON $ defaultOptions
+        { fieldLabelModifier = \x -> case x of
+            "type_" -> "type"
+            _     -> x
+        }
 
+class Fields f where
+  repGetFields :: Proxy (f p) -> [String]
 
-getFields :: Data a => a -> [String]
-getFields = concatMap constrFields . dataTypeConstrs . dataTypeOf
+getFields :: forall a. (Generic a, Fields (Rep a)) => Proxy a -> [String]
+getFields _ = repGetFields @(Rep a) Proxy
+
+instance Fields V1 where repGetFields _ = []
+instance Fields U1 where repGetFields _ = []
+instance Fields (K1 i c) where repGetFields _ = []
+instance (Fields f) => Fields (M1 D c f) where
+  repGetFields _ = repGetFields @f Proxy
+instance (Fields f) => Fields (M1 C c f) where
+  repGetFields _ = repGetFields @f Proxy
+instance (Fields f, Selector c) => Fields (M1 S c f) where
+  repGetFields _ = [selName (undefined :: t c f a)]
+instance (Fields a, Fields b) => Fields (a :*: b) where
+  repGetFields _ = repGetFields @a Proxy <> repGetFields @b Proxy
 
 
 callApi :: FromJSON b => Request -> IO (Either JSONException b)
@@ -63,7 +80,7 @@ getUpcoming url = do
         Right x -> pure $ Just x
 
 fetchVideo :: String -> IO (Maybe Video)
-fetchVideo vidId = getUpcoming $ baseUrl <> "videos/" <> vidId <> "?fields=" <> intercalate "," (getFields (undefined :: Video))
+fetchVideo vidId = getUpcoming $ baseUrl <> "videos/" <> vidId <> "?fields=" <> intercalate "," (getFields @Video Proxy)
     where baseUrl = "https://invidious.snopyta.org/api/v1/"
 
 openMpv :: Maybe String -> IO ()
