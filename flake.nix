fd43565485d3f1ea4ef278dc21c56ad5f0fa8096
dimchee <dimchee90@gmail.com>
2022-01-24 22:50:39 +0100

    Clean flake.nix

--- flake.nix
+++ flake.nix
@@ -1,51 +1,34 @@
 {
-  outputs = { self, nixpkgs }: {
-    # devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
-    #   haskellPackages.developPackage {
-    #     root = ./.;
-    #     modifier = drv:
-    #     haskell.lib.addBuildTools drv (with haskellPackages;
-    #     [ cabal-install
-    #       ghcid
-    #     ]);
-    #   };
-
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation rec {
-        ldpath = lib.makeLibraryPath buildInputs;
-        buildInputs = [ (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
-          lens
-          generic-lens
-          aeson
-          http-conduit
-          optparse-generic
-          turtle
-        ]))
-          gnumake
-          patchelf
-          zlib
-          gmp
-          libffi
-        ];
-        name = "invicli";
-        src = self;
-        buildPhase = "make";
-        installPhase = ''
-            mkdir -p $out/bin; install -t $out/bin invicli
-            patchelf --set-rpath ${ldpath} $out/bin/invicli
-        '';
-      };
-    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
-      mkShell {
-        buildInputs = [ (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
-          lens
-          generic-lens
-          aeson
-          http-conduit
-          optparse-generic
-          turtle
-        ])) ];
-      };
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    myghc = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs;
+    [ lens
+      generic-lens
+      aeson
+      http-conduit
+      optparse-generic
+      turtle
+    ]);
+  in with pkgs; {
+    defaultPackage.x86_64-linux = stdenv.mkDerivation rec {
+      ldpath = lib.makeLibraryPath buildInputs;
+      buildInputs =
+      [ myghc
+        gnumake
+        patchelf
+        zlib
+        gmp
+        libffi
+      ];
+      name = "invicli";
+      src = self;
+      buildPhase = "make";
+      installPhase = ''
+          mkdir -p $out/bin; install -t $out/bin invicli
+          patchelf --set-rpath ${ldpath} $out/bin/invicli
+      '';
+    };
+    devShell.x86_64-linux = mkShell { buildInputs = [ myghc gnumake]; };
   };
 }
